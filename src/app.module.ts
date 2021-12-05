a3092ed347209a65aaced617924d1ebd50cc7dca
vbelchior <victor@thau.tech>
2021-12-05 04:03:02 -0300

    Teste postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { SecretModel } from './secret/secret.model';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SecretModel } from "./secret/secret.model";
 
-import { SecretModule } from './secret/secret.module';
+import { SecretModule } from "./secret/secret.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
+      type: "postgres",
+      host: "ec2-18-210-159-154.compute-1.amazonaws.com",
       port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'challenge',
-      schema: 'public',
+      username: "kuytlwpszqqpou",
+      password:
+        "cc560e868027f2f2575c26d4ba892a32512142edf3c5ad43611e05d56ad08638",
+      database: "d4g8i3s6l788hm",
+      schema: "public",
       entities: [SecretModel],
       synchronize: true,
       autoLoadEntities: true,
