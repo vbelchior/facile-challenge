b3d964ba0f9e6042c2a0bd289f04ce0812c2b5a2
vbelchior <victor@thau.tech>
2021-12-05 13:55:45 -0300

    Adjusts main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const dotenv = require("dotenv");
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
