a68984a438c849abcb943891fafd653f13a2e2de
vbelchior <victor@thau.tech>
2021-12-05 02:22:48 -0300

    Adjusts encript service

--- src/secret/encript.service.ts
+++ src/secret/encript.service.ts
@@ -1,36 +1,36 @@
-import { Cipher, createCipheriv, createDecipheriv, randomBytes } from 'crypto';
+import { Cipher, createCipheriv, createDecipheriv, randomBytes } from "crypto";
 
-import { Injectable } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+import { Injectable } from "@nestjs/common";
+import { Observable, of } from "rxjs";
 
 @Injectable()
 export class EncriptService {
-  private static readonly algorithm: string = 'aes-256-cbc';
-  private static randomKey: Buffer = randomBytes(32);
-  private static initVector: Buffer = randomBytes(16);
+  private static readonly algorithm: string = "aes-256-cbc";
+  private static readonly randomKey: Buffer = randomBytes(32);
+  private static readonly initVector: Buffer = randomBytes(16);
 
   public encript(text: string): Observable<string> {
     const cipher: Cipher = createCipheriv(
       EncriptService.algorithm,
       EncriptService.randomKey,
-      EncriptService.initVector,
+      EncriptService.initVector
     );
 
-    let encryptedData = cipher.update(text, 'utf-8', 'hex');
-    encryptedData += cipher.final('hex');
+    let encryptedData = cipher.update(text, "utf-8", "hex");
+    encryptedData += cipher.final("hex");
 
     return of(encryptedData);
   }
 
-  public decrypt(text): Observable<string> {
+  public decrypt(text: string): Observable<string> {
     const decipher: Cipher = createDecipheriv(
       EncriptService.algorithm,
       EncriptService.randomKey,
-      EncriptService.initVector,
+      EncriptService.initVector
     );
 
-    let decryptedData = decipher.update(text, 'hex', 'utf-8');
-    decryptedData += decipher.final('utf8');
+    let decryptedData = decipher.update(text, "hex", "utf-8");
+    decryptedData += decipher.final("utf8");
 
     return of(decryptedData);
   }
--- src/secret/secret.controller.ts
+++ src/secret/secret.controller.ts
@@ -7,18 +7,19 @@ import {
   Param,
   Post,
   Query,
-} from '@nestjs/common';
-import { firstValueFrom, of } from 'rxjs';
-import { TypeUtil } from 'src/utils/type.util';
-import { EncriptService } from './encript.service';
-import { SecretModel } from './secret.model';
-import { SecretService } from './secret.service';
+  Render,
+} from "@nestjs/common";
+import { firstValueFrom, of } from "rxjs";
+import { TypeUtil } from "src/utils/type.util";
+import { EncriptService } from "./encript.service";
+import { SecretModel } from "./secret.model";
+import { SecretService } from "./secret.service";
 
-@Controller('secrets')
+@Controller("secrets")
 export class SecretController {
   constructor(
     private encriptService: EncriptService,
-    private secretService: SecretService,
+    private secretService: SecretService
   ) {}
 
   @Post()
@@ -26,27 +27,27 @@ export class SecretController {
     try {
       if (!TypeUtil.hasText(secret.name)) throw console.error();
       const encriptedData: string = await firstValueFrom(
-        this.encriptService.encript(secret.name),
+        this.encriptService.encript(secret.name)
       );
       secret.name = encriptedData;
       return firstValueFrom(this.secretService.create(secret));
     } catch (err) {
       throw new HttpException(
         'O campo "name" é obrigatório',
-        HttpStatus.BAD_REQUEST,
+        HttpStatus.BAD_REQUEST
       );
     }
   }
 
-  @Get('/:id')
-  public async retrieve(@Param('id') id: number): Promise<SecretModel> {
+  @Get("/:id")
+  public async retrieve(@Param("id") id: number): Promise<SecretModel> {
     try {
       const secret: SecretModel = await firstValueFrom(
-        this.secretService.retrieve(id),
+        this.secretService.retrieve(id)
       );
       if (!TypeUtil.exists(secret)) throw console.error();
       const decriptedData = await firstValueFrom(
-        this.encriptService.decrypt(secret.name),
+        this.encriptService.decrypt(secret.name)
       );
       delete secret.id;
       secret.name = decriptedData;
@@ -54,13 +55,13 @@ export class SecretController {
     } catch (err) {
       throw new HttpException(
         'O campo "name" é obrigatório',
-        HttpStatus.BAD_REQUEST,
+        HttpStatus.BAD_REQUEST
       );
     }
   }
 
   @Get()
-  public filter(@Query('name') name?: string): Promise<SecretModel[]> {
+  public filter(@Query("name") name?: string): Promise<SecretModel[]> {
     return firstValueFrom(this.secretService.filter(name));
   }
 }
