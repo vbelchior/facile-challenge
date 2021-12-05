aa7c65866a71ad4ec5cedac1f0753359e00236c0
vbelchior <victor@thau.tech>
2021-12-05 13:39:10 -0300

    Adjusts port listen

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const dotenv = require("dotenv");
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
